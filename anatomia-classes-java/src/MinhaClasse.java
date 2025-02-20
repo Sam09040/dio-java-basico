da2d2e1c23cc16bb8626dab4a9627a0193846029
Sam09040 <jpalmeida0904@gmail.com>
2025-02-20 11:02:49 -0400

    variáveis e métodos

---
+++ anatomia-classes-java/src/MinhaClasse.java
@@ -0,0 +1,27 @@
+public class MinhaClasse {
+    // arquivo deve começar com letra maiúscula e a classe deve ter o mesmo nome
+
+    /*
+     * para declarar um método, é necessário utilizar a seguinte estrutura: 
+     * tipoRetorno nomeObjetivoNoInfinitivo Parametro(s)
+     * também é possível utilizar modificador de acesso
+     */
+    public static void main(String[] args) {
+        /* 
+        variáveis devem utilizar o padrão camelCase, onde a primeira palavra começa com letra minúscula e as próximas começam com letra maiúscula 
+        */
+        /* 
+        para declarar uma variável, a estrutura é a seguinte:
+        tipo nomeVariavel = valorInicial; (opcional em alguns casos)
+        */
+        String primeiroNome = "Sam";
+        String segundoNome = "de Almeida";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
